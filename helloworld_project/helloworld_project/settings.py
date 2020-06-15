e3acb1543c35f37b15a0747f5b718777b69954d7
little-peter-pan <xuyi1309@gmail.com>
2020-06-15 18:23:31 -0500

    Prep

--- helloworld_project/helloworld_project/settings.py
+++ helloworld_project/helloworld_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'oe917y2db(3!s%bgm=jdbn6lm^wxa5n#zd3l$)e_uoz-@jx7z)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'Insta',
     'imagekit',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
